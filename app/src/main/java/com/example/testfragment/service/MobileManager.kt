0ef9744cde022b30bc2c4987e9706078077b0135
Nura-ainee Yako <tanylap_sath@hotmail.com>
2019-09-17 14:54:41 +0700

    create manager and apiService file to sync with api

---
+++ app/src/main/java/com/example/testfragment/service/MobileManager.kt
@@ -0,0 +1,20 @@
+package com.example.testfragment.service
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MobileManager {
+
+    companion object {
+        const val MOBILE_LIST_API = "https://scb-test-mobile.herokuapp.com/"
+    }
+
+    fun createService(): ApiService =
+        Retrofit.Builder()
+            .baseUrl(MOBILE_LIST_API)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .run { create(ApiService::class.java) }
+
+
+}
