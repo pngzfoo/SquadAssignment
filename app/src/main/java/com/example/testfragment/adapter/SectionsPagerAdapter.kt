794722041801c5bc0c04a735ec4ee51b9f9f0b9f
Nura-ainee Yako <tanylap_sath@hotmail.com>
2019-09-17 17:50:54 +0700

    update SectionPagerAdapter

--- app/src/main/java/com/example/testfragment/adapter/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/testfragment/adapter/SectionsPagerAdapter.kt
@@ -1,25 +1,46 @@
 package com.example.testfragment.adapter
 
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.example.testfragment.model.FragmentModel
+import com.example.testfragment.model.MobileModel
 
-class SectionsPagerAdapter(private val fmList: List<FragmentModel>, fm: FragmentManager) : FragmentPagerAdapter(fm) {
+//class SectionsPagerAdapter(private val fmList: List<FragmentModel>, fm: FragmentManager) : FragmentPagerAdapter(fm) {
+//
+//    override fun getItem(position: Int): Fragment {
+//        return fmList[position].fragment
+//    }
+//
+//
+//    override fun getPageTitle(position: Int): CharSequence? {
+//        return fmList[position].tabName
+//    }
+//
+//    // size ขนาดเท่าไหร่
+//    override fun getCount(): Int {
+//        return fmList.count()
+//    }
+//
+//
+//}
 
-    override fun getItem(position: Int): Fragment {
-        return fmList[position].fragment
+class SectionsPagerAdapter(
+    private val mobileList: List<MobileModel>
+//    ,private val listener: MobileItemClickListener
+) : RecyclerView.Adapter<MobileListHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MobileListHolder {
+        return MobileListHolder(parent)
     }
 
+    override fun getItemCount(): Int = mobileList.count()
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return fmList[position].tabName
+    override fun onBindViewHolder(holder: MobileListHolder, position: Int) {
+          holder.bind(mobileList[position])
+//        holder.bind(mobileList[position], listener)
     }
-
-    // size ขนาดเท่าไหร่
-    override fun getCount(): Int {
-        return fmList.count()
-    }
-
 
 }
