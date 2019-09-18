dae0888aa028641072b14060f891317bdb4a3265
Nura-ainee Yako <tanylap_sath@hotmail.com>
2019-09-18 15:35:13 +0700

    create mobile detail

---
+++ app/src/main/java/com/example/testfragment/ui/main/main/mobile_detail/MobileDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.testfragment.ui.main.main.mobile_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testfragment.R
+
+class MobileDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mobile_detail)
+    }
+}
